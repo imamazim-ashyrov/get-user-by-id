49dc37e8a4c9b5eda1f2e457f68a411f070270cc
imamazim-ashyrov <kaliev2004kg@gmail.com>
2025-01-16 12:11:27 +0600

    fixed features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { useState } from "react"
 import GetUsers from "./GetUsers"
 
 const App = () => {
-  const [state, setState] = useState(true)
   return (
     <center>
-      {state && <GetUsers />}
+      <GetUsers />
     </center>
   )
 }
